da0985334b1440411ccbbf3b94b3a19f79062b48
Bryan McCarty <bryan.mccarty@turingsaas.com>
2025-06-05 10:09:56 -0400

    update 1 remove old

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OutcomePrioritizationMajor from "./pages/main_flow_major";
-import ChipAllocator from "./pages/chips";
 import OutcomeRankingIntro from "./pages/instructions";
 import OutcomePrioritizationMinor from "./pages/main_flow_minor_new";
 import OutcomePrioritizationCost from "./pages/main_flow_cost_new";
@@ -10,8 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<OutcomeRankingIntro />} />
-        <Route path="/chips" element={<ChipAllocator />} />
-
         <Route path="/prioritize-1" element={<OutcomePrioritizationMajor />} />
         <Route path="/prioritize-2" element={<OutcomePrioritizationCost />} /> {/* NEW */}
         <Route path="/prioritize-3" element={<OutcomePrioritizationMinor />} /> {/* NEW */}
